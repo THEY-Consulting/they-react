a0dbf3436f9193f8bcf421d6266a8bb345fca914
Thiemo <thiemo.gries@they-consulting.de>
2024-12-12 12:54:46 +0100

    setup storybook

---
+++ src/modules/clipboard/CopyToClipboardIconButton.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PetsIcon from '@mui/icons-material/Pets';
+import { CopyToClipboardIconButton } from '../../../lib/main';
+
+const meta = {
+  title: 'Clipboard/CopyToClipboardIconButton',
+  component: CopyToClipboardIconButton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    size: { control: 'select', options: ['small', 'medium', 'large'] },
+    fontSize: { control: 'select', options: ['small', 'medium', 'large'] },
+  },
+  args: {
+    value: 'Hello, world!',
+    hoverMessage: 'Copy to clipboard',
+    successMessage: 'Copied!',
+  },
+} satisfies Meta<typeof CopyToClipboardIconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+    fontSize: 'small',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium',
+    fontSize: 'medium',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    fontSize: 'large',
+  },
+};
+export const NoValue: Story = {
+  args: {
+    value: undefined,
+  },
+};
+
+export const CustomIcon: Story = {
+  args: {
+    children: <PetsIcon />,
+  },
+};
+
+export const CustomHover: Story = {
+  args: {
+    hoverMessage: 'You should copy this!',
+  },
+};
