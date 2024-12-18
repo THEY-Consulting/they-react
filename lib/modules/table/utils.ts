766159dc790b6c801acb00725382db2e9726e2c7
Thiemo <thiemo.gries@they-consulting.de>
2024-12-18 13:55:42 +0100

    add sticky table stories

--- lib/modules/table/utils.ts
+++ lib/modules/table/utils.ts
@@ -2,18 +2,21 @@ import { Path, PathValue } from 'react-hook-form';
 import { get } from 'lodash';
 import { TableColumnFormatter } from './types';
 import { ReactNode } from 'react';
+import { TFunction } from 'i18next';
 
 export const getValue = <T, P extends Path<T>>(data: T, path: P): PathValue<T, P> => {
   return get<T, P>(data, path) as PathValue<T, P>;
 };
 
 export const formatValue = <T, P extends Path<T>>(
+  t: TFunction,
   value: PathValue<T, P>,
   format: TableColumnFormatter<T, P> | undefined,
   empty?: ReactNode
 ): ReactNode => {
+  console.log(t, value, format);
   if (value === null || value === undefined) {
     return empty ?? '-';
   }
-  return (format ? format(value) : value.toString()) || (empty ?? '-');
+  return (format ? format(value, t) : value.toString()) || (empty ?? '-');
 };
