1c5b9045222b1f66223b8bc383c596e667547d72
thiemogries <thiemogries@users.noreply.github.com>
2025-01-14 11:43:58 +0000

    Deploying to gh-pages from @ THEY-Consulting/they-react@bd31e4901a47dd04ef018b985a181efa2d1e060f 🚀

---
+++ assets/ErrorDisplay.stories-aC1MW47T.js
@@ -0,0 +1,5 @@
+import"./index-YTOF1_EJ.js";import{a}from"./StickyTable-DBKKC0wM.js";import"./index-BWfIisTF.js";import"./index-B3O5BJN8.js";const p={title:"error/ErrorDisplay",component:a,parameters:{layout:"centered"},argTypes:{},args:{}},r={args:{error:new Error("This is an error message")}};var e,o,s;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    error: new Error('This is an error message')
+  }
+}`,...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const c=["WithError"];export{r as WithError,c as __namedExportsOrder,p as default};
