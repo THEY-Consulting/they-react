41204e8081a63cd35da91efc4379eac49ea73c3f
Thiemo <thiemo.gries@they-consulting.de>
2025-01-15 14:26:44 +0100

    new option to handle automatic redirects to login page it the user is unauthorized

---
+++ lib/modules/api/types.ts
@@ -0,0 +1,10 @@
+export interface QueryMeta extends Record<string, unknown> {
+  redirectToLocationIfUnauthorized?: string | boolean;
+}
+
+declare module '@tanstack/react-query' {
+  interface Register {
+    queryMeta: QueryMeta;
+    mutationMeta: QueryMeta;
+  }
+}
