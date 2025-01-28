db844fcbee99ea44d6c1145a719695b7f5798a96
Thiemo <thiemo.gries@they-consulting.de>
2025-01-28 13:19:47 +0100

    added stories for ButtonField and CheckboxField

---
+++ src/modules/form/CheckboxField.stories.tsx
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { FormFactory } from '../../../lib/main';
+
+type Entity = {
+  checkbox: boolean;
+};
+
+const meta = {
+  title: 'form/CheckboxField',
+  component: FormFactory,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {},
+  args: {
+    fields: [
+      {
+        fields: [
+          {
+            type: 'boolean',
+            name: 'checkbox',
+            label: 'Label',
+          },
+        ],
+      },
+    ],
+  },
+} satisfies Meta<typeof FormFactory<Entity>>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Checkbox: Story = {};
+
+export const WithoutLabel: Story = {
+  args: {
+    fields: [
+      {
+        fields: [
+          {
+            type: 'boolean',
+            name: 'checkbox',
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    fields: [
+      {
+        fields: [
+          {
+            type: 'boolean',
+            name: 'checkbox',
+            label: 'Label',
+            disabled: true,
+          },
+        ],
+      },
+    ],
+  },
+};
