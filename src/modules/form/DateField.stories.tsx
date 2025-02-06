ea15bab2608c11ff41313f490e9f218fa4e9d3d1
Thiemo <thiemo.gries@they-consulting.de>
2025-02-06 13:32:15 +0100

    added stories for DateField

---
+++ src/modules/form/DateField.stories.tsx
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { enGB } from 'date-fns/locale/en-GB';
+import { FormFactory } from '../../../lib/main';
+
+type Entity = {
+  date: string;
+};
+
+const meta = {
+  title: 'form/DateField',
+  component: FormFactory,
+  decorators: (Story) => (
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enGB}>
+      <Story />
+    </LocalizationProvider>
+  ),
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {},
+  args: {
+    fields: [
+      {
+        fields: [
+          {
+            type: 'date',
+            name: 'date',
+            label: 'Created At',
+          },
+        ],
+      },
+    ],
+  },
+} satisfies Meta<typeof FormFactory<Entity>>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DateField: Story = {};
+
+export const WithoutLabel: Story = {
+  args: {
+    fields: [
+      {
+        fields: [
+          {
+            type: 'date',
+            name: 'date',
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    fields: [
+      {
+        fields: [
+          {
+            type: 'date',
+            name: 'date',
+            label: 'Created At',
+            disabled: true,
+          },
+        ],
+      },
+    ],
+  },
+};
