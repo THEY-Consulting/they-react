1ea31f7c2bdea10a6f99a54a6a87de83c94997e5
Thiemo <thiemo.gries@they-consulting.de>
2025-06-16 15:25:55 +0200

    add support for custom headers in API requests (#64)
    * add support for custom headers in API requests
    
    * fix: ensure headers function returns an empty object if undefined
    
    * pr feedback

--- lib/modules/api/types.ts
+++ lib/modules/api/types.ts
@@ -1,5 +1,6 @@
 export interface QueryMeta extends Record<string, unknown> {
   redirectToLocationIfUnauthorized?: string | boolean;
+  headers?: Record<string, string> | (() => Record<string, string>) | (() => Promise<Record<string, string>>);
 }
 
 declare module '@tanstack/react-query' {
