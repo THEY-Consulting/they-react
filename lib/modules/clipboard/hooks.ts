ec57da60c66937d62ca9d38edda739f9346fa59b
Thiemo <thiemo.gries@they-consulting.de>
2025-06-23 13:27:26 +0200

    add async support for clipboard copy and show progress message (#67)
    * add async support for clipboard copy and show progress message
    
    bump version to v0.16.0
    
    * fix: update import statement for useCopyToClipboard hook

--- lib/modules/clipboard/hooks.ts
+++ lib/modules/clipboard/hooks.ts
@@ -1,12 +1,16 @@
 import { useCallback, useState } from 'react';
 
-export const useCopyToClipboard = (value?: string) => {
+export const useCopyToClipboard = (value?: string | (() => Promise<string> | string)) => {
   const [copied, setCopied] = useState(false);
+  const [inProgress, setInProgress] = useState(false);
 
   const handleClick = useCallback(async () => {
     if (value) {
+      setInProgress(true);
+      // If value is a function, call it to get the string to copy
+      const textToCopy = typeof value === 'function' ? await value() : value;
       try {
-        await navigator.clipboard.writeText(value);
+        await navigator.clipboard.writeText(textToCopy);
         setCopied(true);
         setTimeout(() => {
           setCopied(false);
@@ -14,8 +18,9 @@ export const useCopyToClipboard = (value?: string) => {
       } catch (error) {
         console.error('Failed to copy to clipboard', error);
       }
+      setInProgress(false);
     }
-  }, [value, setCopied]);
+  }, [value]);
 
-  return { copied, handleClick };
+  return { copied, handleClick, inProgress };
 };
