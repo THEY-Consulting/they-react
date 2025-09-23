9958190711a1f5838e32b108a8eede1192761da6
J-Bockhofer <J-Bockhofer@users.noreply.github.com>
2025-09-23 09:50:10 +0000

    Deploying to gh-pages from @ THEY-Consulting/they-react@36db1ea6b93005d54fbc3189a9bf37f58a714182 🚀

--- assets/FormMetaData.stories-BAvEXLB2.js
+++
@@ -1,9 +0,0 @@
-import"./index-DUAV1Q2A.js";import{F as n}from"./FormFactory-C4wSxtBe.js";import"./Map-CKR1jYzf.js";import"./index-DlYN8Jxj.js";import"./index-CqVsPxxY.js";const l={title:"form/FormMetaData",component:n,parameters:{layout:"centered"},argTypes:{},args:{formData:{name:"John Doe",createdAt:"2021-01-01",createdBy:"Jack Doe",editedAt:"2021-01-02",editedBy:"Jane Doe"},fields:[{fields:[{type:"string",name:"name",label:"Name"}]}]}},e={},a={args:{formData:{name:"John Doe",createdAt:"2021-01-01",createdBy:"Jack Doe"}}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var s,m,c;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    formData: {
-      name: 'John Doe',
-      createdAt: '2021-01-01',
-      createdBy: 'Jack Doe'
-    }
-  }
-}`,...(c=(m=a.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const f=["MetaData","CreatedBy"];export{a as CreatedBy,e as MetaData,f as __namedExportsOrder,l as default};
---
+++ assets/FormSubmit.stories-D1MR47ME.js
@@ -0,0 +1,18 @@
+import{j as o}from"./index-BAINcZ39.js";import"./index-DQLiH3RP.js";import{F as x,B as y}from"./FormFactory--pUYyBn8.js";import"./Map-UCBDsxim.js";import{f as S}from"./index-D_Ss_HUe.js";import"./index-CJPVTaBz.js";const w={title:"form/FormSubmit",component:x,decorators:f=>o.jsx(y,{sx:{width:500,height:300},children:o.jsx(f,{})}),parameters:{layout:"centered"},argTypes:{},args:{onSubmit:S(),fields:[{fields:[{type:"string",name:"name",label:"Name"}]}]}},r={},t={args:{hideDirtyNotification:!0}},e={args:{disabled:!0}},s={args:{submitProps:{label:"Check this out!",sx:{width:"100%"}}}};var a,i,m;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var n,c,u;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    hideDirtyNotification: true
+  }
+}`,...(u=(c=t.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var d,p,l;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var g,h,b;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    submitProps: {
+      label: 'Check this out!',
+      sx: {
+        width: '100%'
+      }
+    }
+  }
+}`,...(b=(h=s.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};const k=["Submit","WithoutDirtyNotification","Disabled","CustomButton"];export{s as CustomButton,e as Disabled,r as Submit,t as WithoutDirtyNotification,k as __namedExportsOrder,w as default};
